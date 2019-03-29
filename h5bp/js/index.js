c693c97937121835c4d93373c732bda12e9c59d1
CXF <1360014230@qq.com>
2019-03-29 20:41:14 +0800

    工作完成了

--- h5bp/js/index.js
+++ h5bp/js/index.js
@@ -1,5 +1,5 @@
 $(function(){
-//	黑马下拉块
+//	导航条黑马下拉块
 	$("#heiMa").mouseenter(function(){
 		$("#heiMa_show").show();
 		$("#heiMa_show").mouseenter(function(){
@@ -14,26 +14,26 @@ $(function(){
 	})
 	
 //============================================
-//	查看更多
+//	活动查看更多
 	$("#con_hide").hide();
 	$("#look_more").click(function(){
 		$("#con_hide").show();
 	})
 
 //=================================================================
-    //轮播图
+    //首页轮播图
 	$('.carousel').carousel({
 	  interval: 2000
 	})
 	
 //=======================================================
-	//tab切换已落地城市
+	//首页tab切换已落地城市
 	$(".already_zi_hover>li").mouseenter(function(){
 		$(this).addClass("already_selected").siblings("li").removeClass("already_selected");
 		$(".already_img li").eq($(this).index()).addClass("already_img_show").siblings("li").removeClass("already_img_show");
 	})
 	
-	//已落地城市蒙版
+	//首页已落地城市蒙版
 	$(".mengBan_row").find("div").css("opacity",0.7);
 	$(".mengBan_row").find("div").mouseenter(function(){
 		$(this).css("opacity",1).siblings("div").css("opacity",0.7);
@@ -42,15 +42,22 @@ $(function(){
 		$(this).children("div").css("opacity",0.7);
 	})
 	
-	//热点滚动的字
+	//hot热点滚动的字
 	$(document).ready(function(){
 	     $("#scrollDiv").Scroll({line:1,speed:500,timer:2000});
 	});
 	
+//	hot加载更多
+	$(".hot_news_hide").hide();
+	$(".hot_more").click(function(){
+		$(".hot_news_hide").show();
+	})
 	
-	
-	
-	
+//	hot右下角固定定位
+	$('.fixed_bai').hide();
+	$(window).scroll(function() {
+		$(document).scrollTop() >= 1070 ? $('.fixed_bai').show() : $('.fixed_bai').hide();
+	})
 	
 	
 	
